6122105c6a89136c87a973bdcfb92397d22ef9db
Eugene-Murray <eugene.murray@gmail.com>
2013-11-06 12:16:29 +0000

    add code
    KO / Durandal / Amplify

---
+++ EM.Cloud.KO_CMS/Controllers/DurandalController.cs
@@ -0,0 +1,9 @@
+using System.Web.Mvc;
+
+namespace EM.Cloud.KO_CMS.Controllers {
+  public class DurandalController : Controller {
+    public ActionResult Index() {
+      return View();
+    }
+  }
+}
